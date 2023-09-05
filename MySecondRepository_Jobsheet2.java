dc6429d4fbbd09c4db7c3946f630ae4ae6703a83
zidnafaz <zidnafaza27@gmail.com>
2023-09-05 17:30:59 +0700

    Upload File

---
+++ MySecondRepository_Jobsheet2.java
@@ -0,0 +1,5 @@
+public class MySecondRepository_Jobsheet2{
+     public static void main(String[] args) {
+        System.out.println("\nSecond Repository\n\nI write this repository for my assignment in State Polytechnic of Malang. As a new student in Information Technology, Business Information System Study Program, i'm so excited to explore about programming.\n ");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Second_repository
+
+Welcome To My Second Repository
+
+    I write this repository for my assignment in State Polytechnic of Malang. As a new student in Information Technology, Business Information System Study Program, i'm so excited to explore about programming.
