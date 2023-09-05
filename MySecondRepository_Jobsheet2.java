d84d3bd5d1354124032b0b059977f1d464b20fa1
zidnafaz <zidnafaza27@gmail.com>
2023-09-05 17:44:30 +0700

    Perubahan spasi pada text README.md dan MySecondRepository_Jobsheet2.java

--- MySecondRepository_Jobsheet2.java
+++ MySecondRepository_Jobsheet2.java
@@ -1,5 +1,7 @@
 public class MySecondRepository_Jobsheet2{
      public static void main(String[] args) {
-        System.out.println("\nSecond Repository\n\nI write this repository for my assignment in State Polytechnic of Malang. As a new student in Information Technology, Business Information System Study Program, i'm so excited to explore about programming.\n ");
+        System.out.println("\nSecond Repository\n\nI write this repository for my assignment in State Polytechnic of Malang.");
+        System.out.println("As a new student in Information Technology, Business Information System Study Program,");
+        System.out.println("i'm so excited to explore about programming.\n");
     }
 }
